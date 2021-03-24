57111718c09819e86024ed50917c8d0bd12f4317
felipe.sulzbach <feliperpdr@yahoo.com.br>
2021-03-24 13:13:04 -0300

    Add functions

---
+++ script/functions/cfgfunctions.hpp
@@ -0,0 +1,10 @@
+class MISSION {
+    tag = "MISSION";
+
+    class intro {
+        file = "script\player_animation";
+        class start_brifering {description = "start hq npcs brifering"};
+        class shotdown {description = "Explodes vehicle";};
+        class launchmissle {description = "launches a Guided Missile"};
+    };
+};
